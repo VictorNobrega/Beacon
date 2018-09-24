b561a4562e4c1c2b98b7af061ab2bda29349210c
VictorNobrega <victorpnobrega@gmail.com>
2018-09-24 10:39:32 -0300

    telas

--- src/pages/events/events.ts
+++ src/pages/events/events.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { MenuPage } from '../menu/menu';
 
 /**
- * Generated class for the RegistrationPage page.
+ * Generated class for the EventsPage page.
  *
  * See https://ionicframework.com/docs/components/#navigation for more info on
  * Ionic pages and navigation.
@@ -11,20 +10,16 @@ import { MenuPage } from '../menu/menu';
 
 @IonicPage()
 @Component({
-  selector: 'page-registration',
-  templateUrl: 'registration.html',
+  selector: 'page-events',
+  templateUrl: 'events.html',
 })
-export class RegistrationPage {
+export class EventsPage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad RegistrationPage');
-  }
-
-  goToMain() {
-    this.navCtrl.setRoot(MenuPage);
+    console.log('ionViewDidLoad EventsPage');
   }
 
 }
--- src/pages/menu/menu.ts
+++ src/pages/menu/menu.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { AboutPage } from '../about/about';
+import { EventsPage } from '../events/events';
+import { HistoryPage } from '../history/history';
+import { HomePage } from '../home/home';
+import { HoursPage } from '../hours/hours';
+import { MainPage } from '../main/main';
 
 /**
  * Generated class for the MenuPage page.
@@ -22,4 +28,27 @@ export class MenuPage {
     console.log('ionViewDidLoad MenuPage');
   }
 
+  goToHome() {
+    this.navCtrl.push(HomePage);
+  }
+
+  goToMain() {
+    this.navCtrl.push(MainPage);
+  }
+
+  goToHistory() {
+    this.navCtrl.push(HistoryPage);
+  }
+
+  goToHours() {
+    this.navCtrl.push(HoursPage);
+  }
+
+  goToEvents() {
+    this.navCtrl.push(EventsPage);
+  }
+
+  goToAbout() {
+    this.navCtrl.push(AboutPage);
+  }
 }
